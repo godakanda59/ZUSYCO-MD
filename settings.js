0f3d143ee61f2350765d0c1cad5529087d7aab47
godakanda59 <139145269+godakanda59@users.noreply.github.com>
2024-03-27 01:22:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -42,7 +42,7 @@ module.exports = {
   ANTI_LINKS : 'chat.whatsapp.com',
   ANTI_LINK : false,
   ANTI_LINK_KICK : false,
-  MAX_SIZE : 100,
+  MAX_SIZE : 300,
   MOROCCO_NUMBERS : {
         BLOCK : false,
         NOT_WORKING : false,
