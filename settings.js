5e016090342ae77e50421dfcc0e1fdd02090977d
godakanda59 <139145269+godakanda59@users.noreply.github.com>
2024-03-30 18:25:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -52,7 +52,7 @@ module.exports = {
      ONLY_COMMANDS : true,
      STATUS : true,
   },
-  BOT_OFFLINE : true,
+  BOT_OFFLINE : false,
   INBOX_BLOCK : false,
   AUTO_REACT : true,
   OWNER_REACT : '😗',
